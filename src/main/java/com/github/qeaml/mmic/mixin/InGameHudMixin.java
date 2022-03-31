33cecd8f186bd906bc61a6a6ac2f100bc1261817
qeaml <qeaml@protonmail.com>
2022-03-31 21:05:45 +0200

    overhaul InGameHudMixin

--- src/main/java/com/github/qeaml/mmic/mixin/InGameHudMixin.java
+++ src/main/java/com/github/qeaml/mmic/mixin/InGameHudMixin.java
@@ -7,6 +7,7 @@ import com.github.qeaml.mmic.Keys;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -15,6 +16,7 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawableHelper;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
 
 @Mixin(InGameHud.class)
@@ -22,20 +24,57 @@ public class InGameHudMixin {
 	@Final
 	@Shadow
 	private MinecraftClient client;
+	@Shadow
+	private int scaledWidth;
+	@Shadow
+	private int scaledHeight;
+	@Shadow
+	private Text overlayMessage;
+	@Shadow
+	private int overlayRemaining;
+	@Shadow
+	private boolean overlayTinted;
 
-	private boolean fullbright;
-	private double oldGamma;
+	public final String fullbrightText = "FULLBRIGHT";
+	private @Unique boolean fullbright;
+	private @Unique boolean paused;
+	private @Unique double oldGamma;
 
 	@Inject(at = @At("HEAD"), method = "render(Lnet/minecraft/client/util/math/MatrixStack;F)V")
-	private void injectRender(MatrixStack matrices, float tickDelta, CallbackInfo ci) {
-		handleGammaKeys();
+	private void onRender(MatrixStack matrices, float tickDelta, CallbackInfo ci) {
 		for(Grid g: Grid.values()) {
-			handleGridKey(g);
-			if(g.show) drawGrid(matrices, g);
+			if(!g.show) continue;
+			for(Grid.Rect r: g.rects) {
+				int ax = (int)((float)scaledWidth * r.a().x())-1;
+				int ay = (int)((float)scaledHeight * r.a().y())-1;
+				int bx = (int)((float)scaledWidth * r.b().x())+1;
+				int by = (int)((float)scaledHeight * r.b().y())+1;
+				DrawableHelper.fill(matrices, ax, ay, bx, by, Client.gridColor);
+			}
+		}
+
+		if(fullbright && !paused) {
+			int y = scaledHeight - client.textRenderer.fontHeight - 5;
+			client.textRenderer.drawWithShadow(matrices, fullbrightText, 5, y, 0x80FFFFFF);
 		}
 	}
 
-	private void handleGammaKeys() {
+	@Inject(at = @At("HEAD"), method = "tick(Z)V")
+	private void onTick(boolean paused, CallbackInfo ci) {
+		this.paused = paused;
+	}
+
+	@Inject(at = @At("TAIL"), method = "tick()V")
+	private void onTick(CallbackInfo ci) {
+		for(Grid g: Grid.values()) {
+			if(g.toggle.wasJustPressed()) {
+				g.show = !g.show;
+				notify(new TranslatableText("other.mmic.toggled_grid",
+					new TranslatableText("other.mmic.grid."+g.name),
+					Client.onOff(g.show)));
+			}
+		}
+
 		if(Keys.fullbright.wasJustPressed()) {
 			fullbright = !fullbright;
 			if(fullbright) {
@@ -44,39 +83,26 @@ public class InGameHudMixin {
 			} else {
 				client.options.gamma = oldGamma;
 			}
-			Client.notify(new TranslatableText("other.mmic.toggled_fullbright", Client.onOff(fullbright)));
+			notify(new TranslatableText("other.mmic.toggled_fullbright", Client.onOff(fullbright)));
 		}
 
 		if(fullbright) return; // the keys below do not matter to us in fullbright
 
 		if(Keys.gammaInc.wasJustPressed() && client.options.gamma <= 3.0) {
 			client.options.gamma += Client.gammaStep;
-			Client.notify(new TranslatableText("other.mmic.changed_gamma", Math.round(client.options.gamma * 100)));
+			notify(new TranslatableText("other.mmic.changed_gamma", Math.round(client.options.gamma * 100)));
 		}
 		if(Keys.gammaDec.wasJustPressed() && client.options.gamma >= -1.0) {
 			client.options.gamma -= Client.gammaStep;
-			Client.notify(new TranslatableText("other.mmic.changed_gamma", Math.round(client.options.gamma * 100)));
+			notify(new TranslatableText("other.mmic.changed_gamma", Math.round(client.options.gamma * 100)));
 		}
 	}
 
-	private void drawGrid(MatrixStack matrices, Grid g) {
-		for(Grid.Rect r: g.rects) {
-			int w = this.client.getWindow().getScaledWidth();
-			int h = this.client.getWindow().getScaledHeight();
-			int ax = (int)((float)w * r.a().x())-1;
-			int ay = (int)((float)h * r.a().y())-1;
-			int bx = (int)((float)w * r.b().x())+1;
-			int by = (int)((float)h * r.b().y())+1;
-			DrawableHelper.fill(matrices, ax, ay, bx, by, Client.gridColor);
-		}
-	}
-
-	private void handleGridKey(Grid g) {
-		if(g.toggle.wasJustPressed()) {
-			g.show = !g.show;
-			Client.notify(new TranslatableText("other.mmic.toggled_grid",
-				new TranslatableText("other.mmic.grid."+g.name),
-				Client.onOff(g.show)));
-		}
+	@Unique
+	private void notify(Text text) {
+		overlayMessage = text;
+		overlayRemaining = 30;
+		overlayTinted = false;
+		Client.playClick();
 	}
 }
