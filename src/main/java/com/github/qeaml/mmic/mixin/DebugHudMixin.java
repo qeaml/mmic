378ad5f6ff0f4d3da23c56e1380c3697849552cf
qeaml <qeaml@protonmail.com>
2022-04-17 12:56:59 +0200

    use line lists in debug HUD

--- src/main/java/com/github/qeaml/mmic/mixin/DebugHudMixin.java
+++ src/main/java/com/github/qeaml/mmic/mixin/DebugHudMixin.java
@@ -1,5 +1,7 @@
 package com.github.qeaml.mmic.mixin;
 
+import java.util.LinkedList;
+
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -9,7 +11,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import net.minecraft.block.Block;
 import net.minecraft.client.ClientBrandRetriever;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.TextRenderer;
@@ -21,7 +22,6 @@ import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.server.integrated.IntegratedServer;
-import net.minecraft.tag.TagKey;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.util.hit.HitResult;
@@ -56,17 +56,9 @@ public class DebugHudMixin extends DrawableHelper {
 	private void hijackRender(MatrixStack matrices, CallbackInfo ci) {
 		if(!client.options.reducedDebugInfo) return;
 
+		var ll = new LinkedList<String>();
+
 		framecount++;
-		var cam = client.getCameraEntity();
-
-		var n = textRenderer.fontHeight/2;
-		fillGradient(
-			matrices,
-			n, n,
-			50*n+n+n+n,
-			9*textRenderer.fontHeight+n,
-			0x801F1F1f, 0x800F0F0F);
-
 		var delta = System.currentTimeMillis()-lastInfo;
 		if(delta >= 100L) {
 			fps = 1000 * framecount / delta;
@@ -74,6 +66,49 @@ public class DebugHudMixin extends DrawableHelper {
 			framecount = 0;
 			lastInfo = System.currentTimeMillis();
 		}
+		ll.add(String.format("FPS: %d (%dms)", fps, frametime));
+
+		var r = Runtime.getRuntime();
+		ll.add(String.format("Memory: %.02f/%.02fMiB",
+			toMiB(r.totalMemory() - r.freeMemory()),
+			toMiB(r.maxMemory())));
+
+		ll.add(String.format("Client: %s %s (%s)",
+			ClientBrandRetriever.getClientModName(),
+			client.getGameVersion(),
+			client.getVersionType()));
+
+		IntegratedServer is;
+		if((is = client.getServer()) != null)
+			ll.add(String.format("Server: Integrated @ %.2fms", is.getTickTime()));
+		else
+			ll.add(String.format("Server: %s", client.player.getServerBrand()));
+
+		var cam = client.getCameraEntity();
+		ll.add(String.format("XYZ: %.2f/%.2f/%.2f", cam.getX(), cam.getY(), cam.getZ()));
+		ll.add(String.format("Rotation: %.2f/%.2f (%s)", cam.getYaw(), cam.getPitch(), cam.getHorizontalFacing()));
+		ll.add(String.format("Chunk: %d/%d", (int)(cam.getX() / 16.0), (int)(cam.getZ() / 16.0)));
+
+		if(cam.getY() >= client.world.getBottomY() && cam.getY() < client.world.getTopY())
+			ll.add(String.format("Biome: %s", getBiomeString(client.world.getBiome(cam.getBlockPos()))));
+		else
+			ll.add("Out of world.");
+
+		int lbgw = 0;
+		for(String s: ll)
+		{
+			int w = textRenderer.getWidth(s);
+			if(w > lbgw)
+				lbgw = w;
+		}
+
+		int n = textRenderer.fontHeight/2;
+		fillGradient(matrices,
+			n, n,
+			lbgw+textRenderer.fontHeight+n,
+			ll.size()*textRenderer.fontHeight+textRenderer.fontHeight+n,
+			0x801F1F1f, 0x800F0F0F);
+
 		var perf = 0xFF00FF00;
 		if(fps < 20)
 			perf = 0xFFFF0000;
@@ -81,119 +116,94 @@ public class DebugHudMixin extends DrawableHelper {
 			perf = 0xFFFF8000;
 		else if(fps < 60)
 			perf = 0xFFFFFF00;
-		fill(
-			matrices,
-			50*n+n+n+n, n,
-			50*n+n+n+n+n, 9*textRenderer.fontHeight+n,
+		fill(matrices,
+			0, n,
+			n, ll.size()*textRenderer.fontHeight+textRenderer.fontHeight+n,
 			perf);
 
 		leftLines = 0;
-		leftLine(matrices, "FPS: %d (%d ms)", fps, frametime);
-
-		var r = Runtime.getRuntime();
-		leftLine(matrices, "Memory: %.02f/%.02fMiB",
-			toMiB(r.totalMemory() - r.freeMemory()),
-			toMiB(r.maxMemory()));
-
-		leftLine(matrices, "Client: %s %s (%s)",
-			ClientBrandRetriever.getClientModName(),
-			client.getGameVersion(),
-			client.getVersionType());
-
-		IntegratedServer is;
-		if((is = client.getServer()) != null)
-			leftLine(matrices, "Server: Integrated @ %.2fms", is.getTickTime());
-		else
-			leftLine(matrices, "Server: %s", client.player.getServerBrand());
-
-		leftLine(matrices, "XYZ: %.2f/%.2f/%.2f", cam.getX(), cam.getY(), cam.getZ());
-		leftLine(matrices, "Rotation: %.2f/%.2f (%s)", cam.getYaw(), cam.getPitch(), cam.getHorizontalFacing());
-		leftLine(matrices, "Chunk: %d/%d", (int)(cam.getX() / 16.0), (int)(cam.getZ() / 16.0));
-
-		if(cam.getY() >= client.world.getBottomY() && cam.getY() < client.world.getTopY())
-			leftLine(matrices, "Biome: %s", getBiomeString(client.world.getBiome(cam.getBlockPos())));
-		else
-			leftLine(matrices, "Out of world.");
+		ll.forEach(text -> leftLine(matrices, text));
 
 		if(client.crosshairTarget.getType() == HitResult.Type.BLOCK)
 		{
+			var cl = new LinkedList<String>();
 			var pos = ((BlockHitResult)client.crosshairTarget).getBlockPos();
 			var state = client.player.getWorld().getBlockState(pos);
-			var info = String.format("%s at %s", Registry.BLOCK.getId(state.getBlock()), pos.toShortString());
-			var bgwidth = textRenderer.getWidth(info);
+			cl.add(String.format("%s at %s", Registry.BLOCK.getId(state.getBlock()), pos.toShortString()));
+			state.getProperties().forEach(prop -> cl.add(prop.getName()+"="+String.valueOf(state.get(prop))));
+			state.streamTags().forEach(tag -> cl.add("#"+tag.id().toString()));
+			int cbgw = 0;
+			for(String s: cl)
+			{
+				int w = textRenderer.getWidth(s);
+				if(w > cbgw)
+					cbgw = w;
+			}
 			var sw = client.getWindow().getScaledWidth();
 			var sh = client.getWindow().getScaledHeight();
-			var props = state.getProperties();
-			var tags = state.streamTags().toList();
-			for(TagKey<Block> tag: tags)
-			{
-				int w = textRenderer.getWidth("#"+tag.id().toString());
-				if(w > bgwidth)
-					bgwidth = w;
-			}
 			fillGradient
 			(
 				matrices,
 				sw/2
-					-bgwidth/2
+					-cbgw/2
 					-n,
 				sh/2
 					+textRenderer.fontHeight
 					+n,
 				sw/2
-					+bgwidth/2
+					+cbgw/2
 					+n,
 				sh/2
 					+textRenderer.fontHeight
-					+textRenderer.fontHeight
-					+(props.size()+tags.size())*textRenderer.fontHeight
+					+cl.size()*textRenderer.fontHeight
 					+textRenderer.fontHeight
 					+n,
 				0x801F1F1f, 0x800F0F0F
 			);
 			centerLines = 0;
-			centerLine(matrices, info);
-			props.forEach(prop -> centerLine(matrices, "%s=%s", prop.getName(), state.get(prop)));
-			tags.forEach(tag -> centerLine(matrices, "#%s", tag.id()));
+			cl.forEach(text -> centerLine(matrices, text));
 		}
 		else if(client.crosshairTarget.getType() == HitResult.Type.ENTITY)
 		{
+			var cl = new LinkedList<String>();
 			var ent = ((EntityHitResult)client.crosshairTarget).getEntity();
-			var info = String.format("%s at %.2f, %.2f, %.2f",
+			cl.add(String.format("%s at %.2f, %.2f, %.2f",
 				Registry.ENTITY_TYPE.getId(ent.getType()),
-				ent.getPos().x, ent.getPos().y, ent.getPos().z);
-			var infowidth = textRenderer.getWidth(info);
+				ent.getPos().x, ent.getPos().y, ent.getPos().z));
+			if(ent instanceof LivingEntity lent)
+				cl.add(String.format("Health: %.2f/%.2f",
+					lent.getHealth(), lent.getAttributeValue(EntityAttributes.GENERIC_MAX_HEALTH)));
+			//TODO: tags here too?
+			int cbgw = 0;
+			for(String s: cl)
+			{
+				int w = textRenderer.getWidth(s);
+				if(w > cbgw)
+					cbgw = w;
+			}
 			var sw = client.getWindow().getScaledWidth();
 			var sh = client.getWindow().getScaledHeight();
-			//TODO: tags here too?
-			int lines = 0;
-			if(ent instanceof LivingEntity)
-				lines = 1;
 			fillGradient
 			(
 				matrices,
 				sw/2
-					-infowidth/2
+					-cbgw/2
 					-n,
 				sh/2
 					+textRenderer.fontHeight
 					+n,
 				sw/2
-					+infowidth/2
+					+cbgw/2
 					+n,
 				sh/2
 					+textRenderer.fontHeight
-					+textRenderer.fontHeight
-					+lines*textRenderer.fontHeight
+					+cl.size()*textRenderer.fontHeight
 					+textRenderer.fontHeight
 					+n,
 				0x801F1F1f, 0x800F0F0F
 			);
 			centerLines = 0;
-			centerLine(matrices, info);
-			if(ent instanceof LivingEntity lent)
-				centerLine(matrices, "Health: %.2f/%.2f",
-					lent.getHealth(), lent.getAttributeValue(EntityAttributes.GENERIC_MAX_HEALTH));
+			cl.forEach(text -> centerLine(matrices, text));
 		}
 
 		ci.cancel();
@@ -202,10 +212,10 @@ public class DebugHudMixin extends DrawableHelper {
 	private @Unique int leftLines = 0;
 
 	@Unique
-	private void leftLine(MatrixStack matrices, String format, Object... args) {
+	private void leftLine(MatrixStack matrices, String text) {
 		var immediate = VertexConsumerProvider.immediate(Tessellator.getInstance().getBuffer());
 		((TextRendererAccessor)textRenderer).internalDraw(
-			String.format(format, args),
+			text,
 			textRenderer.fontHeight,
 			textRenderer.fontHeight*leftLines+textRenderer.fontHeight,
 			0xFFFFFFFF,
@@ -223,10 +233,9 @@ public class DebugHudMixin extends DrawableHelper {
 	private @Unique int centerLines = 0;
 
 	@Unique
-	private void centerLine(MatrixStack matrices, String format, Object... args)
+	private void centerLine(MatrixStack matrices, String text)
 	{
 		var immediate = VertexConsumerProvider.immediate(Tessellator.getInstance().getBuffer());
-		var text = String.format(format, args);
 		((TextRendererAccessor)textRenderer).internalDraw(
 			text,
 			client.getWindow().getScaledWidth()/2
