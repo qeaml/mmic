d4b282636eb7a0848d180ce2ec0a4b0d283f1257
qeaml <qeaml@protonmail.com>
2022-09-25 23:09:10 +0200

    Make Mod Menu aware of new config screen

---
+++ src/main/java/com/github/qeaml/mmic/gui/ModMenuApiImpl.java
@@ -0,0 +1,11 @@
+package com.github.qeaml.mmic.gui;
+
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+
+public class ModMenuApiImpl implements ModMenuApi {
+  @Override
+  public ConfigScreenFactory<?> getModConfigScreenFactory() {
+    return ConfigScreen::new;
+  }
+}
